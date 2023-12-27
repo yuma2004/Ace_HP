5322f26199897a7f74877dde5c4704d455b2fa2f
ゆうま <101465220+yuma2004@users.noreply.github.com>
2023-12-27 11:38:52 +0900

    始めてのコミット

---
+++ index.js
@@ -0,0 +1,6 @@
+function switchSection(sectionId) {
+    document.querySelectorAll('.section').forEach(section => {
+        section.classList.remove('active');
+    });
+    document.getElementById(sectionId).classList.add('active');
+}
