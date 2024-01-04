c7c3405bfb8d046508ab8234f1a65d0cc5becd7e
L0nginu5 <gochomasa@gmail.com>
2024-01-04 16:09:45 +0900

    アニメーション等

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ function switchSectionHeader() {
     }
 }
 
+function switchSectionMenu() {
+    var section1 = document.getElementById('section1');
+    var section2 = document.getElementById('section2');
+    
+    if (section1.style.display === 'none') {
+        section1.style.display = 'block';
+        section2.style.display = 'none';
+    } else {
+        section1.style.display = 'none';
+        section2.style.display = 'block';
+    }
+
+    // ID 'StoreInformation' を持つ要素を取得
+    var element = document.getElementById("メニュー");
+
+    // その要素へスクロール
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+    // スクロール完了後に少し上にオフセット
+    window.setTimeout(function() {
+        window.scrollBy(0, 0);
+    }, 600); // 500ミリ秒後にスクロールを調整
+}
+
+
 function switchSectionStoreInformation() {
     var section1 = document.getElementById('section1');
     var section2 = document.getElementById('section2');
@@ -28,6 +53,11 @@ function switchSectionStoreInformation() {
 
     // その要素へスクロール
     element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+    // スクロール完了後に少し上にオフセット
+    window.setTimeout(function() {
+        window.scrollBy(0, 0);
+    }, 600); // 500ミリ秒後にスクロールを調整
 }
 
 function switchSectionDetailedInformation() {
@@ -47,6 +77,11 @@ function switchSectionDetailedInformation() {
 
     // その要素へスクロール
     element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+    // スクロール完了後に少し上にオフセット
+    window.setTimeout(function() {
+        window.scrollBy(0, 0);
+    }, 600); // 500ミリ秒後にスクロールを調整
 }
 
 function switchSectionCouse() {
@@ -59,10 +94,13 @@ function switchSectionCouse() {
 
     if (section3.style.display === 'none') {
         section3.style.display = 'block';
+        section3.classList.add('showWithAnimation');
     } else {
         section3.style.display = 'none';
+        section3.classList.remove('showWithAnimation');
     }
 }
+
 
 function switchSectionSingle() {
     var section3 = document.getElementById('section3');
@@ -74,7 +112,17 @@ function switchSectionSingle() {
 
     if (section4.style.display === 'none') {
         section4.style.display = 'block';
+        section4.classList.add('showWithAnimation');
     } else {
         section4.style.display = 'none';
+        section4.classList.remove('showWithAnimation');
     }
 }
+
+document.getElementById("image").addEventListener("mouseover", function() {
+    document.getElementById("text").style.transform = "scale(1.5)";
+});
+
+document.getElementById("image").addEventListener("mouseout", function() {
+    document.getElementById("text").style.transform = "scale(1)";
+});
